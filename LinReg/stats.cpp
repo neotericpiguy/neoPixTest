a2618c739d30708f89aaf40a7e8a0405067bc848
neotericpiguy <neotericpiguy@gmail.com>
2021-07-25 20:59:04 -0700

    lin gnuplot

--- LinReg/stats.cpp
+++ LinReg/stats.cpp
@@ -1,172 +1,149 @@
+#include <stdlib.h>
+
 #include <chrono>
 #include <cmath>
 #include <fstream>
 #include <functional>
 #include <iostream>
 #include <map>
+#include <thread>
 #include <vector>
 
-namespace Reg {
-struct LinRegData {
-  double Sx;
-  double Sy;
-  double Sxx;
-  double Sxy;
-  double Syy;
-  unsigned int n;
-  double m;
-  double b;
-  double r;
+class Stats
+{
+public:
+  struct StatsData {
+    unsigned int xColumn;
+    unsigned int yColumn;
+    double Sx;
+    double Sy;
+    double Sxx;
+    double Sxy;
+    double Syy;
+    unsigned int n;
+    double m;
+    double b;
+    double r;
+  };
+
+  int open(const std::string& filename)
+  {
+    std::ifstream instream(filename.c_str());
+
+    mFilename = filename;
+
+    if (!instream.is_open())
+    {
+      std::cerr << "filename janky" << std::endl;
+      return -1;
+    }
+
+    std::string line;
+    while (!instream.eof())
+    {
+      double data;
+      std::vector<double> datas;
+      do
+      {
+        instream >> data;
+        if (instream.eof())
+          break;
+        datas.push_back(data);
+      } while (instream.get() != '\n');
+      if (instream.eof())
+        break;
+
+      for (auto& pair : mStatPairs)
+      {
+        if (pair.second.xColumn > datas.size() || pair.second.yColumn > datas.size())
+        {
+          std::cerr << "invalid column: " << std::endl;
+          return -1;
+        }
+        addPair(pair.first, datas[pair.second.xColumn - 1], datas[pair.second.yColumn - 1]);
+      }
+    }
+    return 0;
+  }
+
+  int addStatPair(const std::string& key, unsigned int x, unsigned int y)
+  {
+    mStatPairs[key].xColumn = x;
+    mStatPairs[key].yColumn = y;
+    return 0;
+  }
+
+  int addPair(const std::string& key, double x, double y)
+  {
+    mStatPairs[key].Sx += x;
+    mStatPairs[key].Sy += y;
+    mStatPairs[key].Sxx += x * x;
+    mStatPairs[key].Syy += y * y;
+    mStatPairs[key].Sxy += x * y;
+    mStatPairs[key].n++;
+
+    return 0;
+  }
+
+  int LinRegCoefs(const std::string& key)
+  {
+    mStatPairs[key].m = (mStatPairs[key].n * mStatPairs[key].Sxy - mStatPairs[key].Sx * mStatPairs[key].Sy) / (mStatPairs[key].n * mStatPairs[key].Sxx - mStatPairs[key].Sx * mStatPairs[key].Sx);
+    mStatPairs[key].b = mStatPairs[key].Sy / mStatPairs[key].n - mStatPairs[key].m * mStatPairs[key].Sx / mStatPairs[key].n;
+    mStatPairs[key].r = (mStatPairs[key].n * mStatPairs[key].Sxy - mStatPairs[key].Sx * mStatPairs[key].Sy) / sqrt((mStatPairs[key].n * mStatPairs[key].Sxx - mStatPairs[key].Sx * mStatPairs[key].Sx) * (mStatPairs[key].n * mStatPairs[key].Syy - mStatPairs[key].Sy * mStatPairs[key].Sy));
+    return 0;
+  }
+
+  int plot(const std::string& key, const std::string& outputFilename) const
+  {
+    std::ofstream outFile(outputFilename + ".plot");
+    if (!outFile.is_open())
+      return -1;
+
+    const StatsData& statsData = mStatPairs.at(key);
+
+    outFile << "set terminal png size 2000,800;\n";
+    outFile << "set datafile separator \",\";\n";
+    outFile << "set output \"" << outputFilename << "\";\n";
+
+    if (statsData.r != 0)
+    {
+      outFile << "m = " << statsData.m << std::endl;
+      outFile << "b = " << statsData.b << std::endl;
+      outFile << "f(x) = m*x+b" << std::endl;
+    }
+
+    outFile << "plot \"" << mFilename << "\" using " << statsData.xColumn << ":"
+            << statsData.yColumn << " title \"" << key << "\" with linespoints";
+
+    if (statsData.r != 0)
+    {
+      outFile << ", f(x) with line title \"linreg\"";
+    }
+
+    outFile << std::endl;
+    system(std::string("gnuplot -p " + outputFilename + ".plot").c_str());
+    system(std::string("rm " + outputFilename + ".plot").c_str());
+    return 0;
+  }
+
+  const StatsData& getStats(const std::string& key) const
+  {
+    static StatsData empty;
+    if (mStatPairs.find(key) == mStatPairs.end())
+    {
+      return empty;
+    }
+    return mStatPairs.at(key);
+  }
+
+private:
+  std::map<std::string, StatsData> mStatPairs;
+  std::string mFilename;
 };
-
-struct LinRegData_xy {
-  double x;
-  double y;
-  double Sx;
-  double Sy;
-  double Sxx;
-  double Sxy;
-  double Syy;
-  unsigned int n;
-  double m;
-  double b;
-  double r;
-};
-
-void LinReg(LinRegData& data, double x, double y)
-{
-  data.Sx += x;
-  data.Sy += y;
-  data.Sxx += x * x;
-  data.Syy += y * y;
-  data.Sxy += x * y;
-  data.n++;
-}
-
-constexpr void LinRegCoefs(LinRegData& data)
-{
-  data.m = (data.n * data.Sxy - data.Sx * data.Sy) / (data.n * data.Sxx - data.Sx * data.Sx);
-  data.b = data.Sy / data.n - data.m * data.Sx / data.n;
-  data.r = (data.n * data.Sxy - data.Sx * data.Sy) / sqrt((data.n * data.Sxx - data.Sx * data.Sx) * (data.n * data.Syy - data.Sy * data.Sy));
-}
-
-constexpr void LinRegCoefs(LinRegData_xy& data)
-{
-  data.m = (data.n * data.Sxy - data.Sx * data.Sy) / (data.n * data.Sxx - data.Sx * data.Sx);
-  data.b = data.Sy / data.n - data.m * data.Sx / data.n;
-  data.r = (data.n * data.Sxy - data.Sx * data.Sy) / sqrt((data.n * data.Sxx - data.Sx * data.Sx) * (data.n * data.Syy - data.Sy * data.Sy));
-}
-
-void LinReg_xy(LinRegData_xy& data)
-{
-  data.Sx += data.x;
-  data.Sy += data.y;
-  data.Sxx += data.x * data.x;
-  data.Syy += data.y * data.y;
-  data.Sxy += data.x * data.y;
-  data.n++;
-}
-
-}  // namespace Reg
-
-void LinRegStatic(double& m, double& b, double& r, double x, double y)
-{
-  static double Sx = 0;
-  static double Sy = 0;
-  static double Sxx = 0;
-  static double Syy = 0;
-  static double Sxy = 0;
-  static unsigned int n = 0;
-
-  Sx += x;
-  Sy += y;
-  Sxx += x * x;
-  Syy += y * y;
-  Sxy += x * y;
-  n++;
-
-  m = (n * Sxy - Sx * Sy) / (n * Sxx - Sx * Sx);
-  b = Sy / n - m * Sx / n;
-  r = (n * Sxy - Sx * Sy) / sqrt((n * Sxx - Sx * Sx) * (n * Syy - Sy * Sy));
-}
-
-std::chrono::microseconds trial1(std::ifstream& instream)
-{
-  auto start = std::chrono::high_resolution_clock::now();
-  Reg::LinRegData data = {0};
-  double& m = data.m;
-  double& b = data.b;
-  double& r = data.r;
-  while (!instream.eof())
-  {
-    double x;
-    double y;
-
-    instream >> x;
-    instream.get();
-    instream >> y;
-    if (instream.eof())
-      break;
-    Reg::LinReg(data, x, y);
-  }
-  Reg::LinRegCoefs(data);
-  auto stop = std::chrono::high_resolution_clock::now();
-
-  instream.clear();                  // clear fail and eof bits
-  instream.seekg(0, std::ios::beg);  // back to the start!
-
-  return std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-}
-
-std::chrono::microseconds trial2(std::ifstream& instream)
-{
-  auto start = std::chrono::high_resolution_clock::now();
-  double m, b, r;
-  while (!instream.eof())
-  {
-    double x;
-    double y;
-
-    instream >> x;
-    instream.get();
-    instream >> y;
-    if (instream.eof())
-      break;
-    LinRegStatic(m, b, r, x, y);
-  }
-  auto stop = std::chrono::high_resolution_clock::now();
-
-  instream.clear();                  // clear fail and eof bits
-  instream.seekg(0, std::ios::beg);  // back to the start!
-
-  return std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-}
-
-std::chrono::microseconds trial3(std::ifstream& instream)
-{
-  auto start = std::chrono::high_resolution_clock::now();
-  Reg::LinRegData_xy data = {0};
-  while (!instream.eof())
-  {
-    instream >> data.x;
-    instream.get();
-    instream >> data.y;
-    if (instream.eof())
-      break;
-    Reg::LinReg_xy(data);
-  }
-  Reg::LinRegCoefs(data);
-  auto stop = std::chrono::high_resolution_clock::now();
-
-  instream.clear();                  // clear fail and eof bits
-  instream.seekg(0, std::ios::beg);  // back to the start!
-
-  return std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-}
 
 int main(int argc, char** argv)
 {
-  if (argc < 1)
+  if (argc < 2)
     return -1;
 
   std::string filename(argv[1]);
@@ -180,27 +157,15 @@ int main(int argc, char** argv)
     return -1;
   }
 
-  const int repeats = 100;
-  std::cout << "repeats: " << repeats << std::endl;
-
-  const std::map<std::string, std::function<std::chrono::microseconds(std::ifstream&)>> funcMap = {
-      {"data   ", trial1},
-      {"static ", trial2},
-      {"data_xy", trial3},
-  };
-  std::map<std::string, std::chrono::microseconds> durationMap;
-
-  for (int i = 0; i < repeats; ++i)
-  {
-    for (const auto& funcPair : funcMap)
-      durationMap[funcPair.first] += funcPair.second(instream);
-
-    instream.clear();                  // clear fail and eof bits
-    instream.seekg(0, std::ios::beg);  // back to the start!
-  }
-
-  for (const auto& durationPair : durationMap)
-    std::cout << "\"" << durationPair.first << "\": " << durationPair.second.count() / repeats << "us" << std::endl;
+  Stats data;
+  data.addStatPair("data", 1, 2);
+  data.open(filename);
+  data.LinRegCoefs("data");
+  const Stats::StatsData& linData = data.getStats("data");
+  std::cout << linData.m << std::endl;
+  std::cout << linData.b << std::endl;
+  data.plot("data", "data.png");
+  //  data.plotLinReg("file.plot");
 
   return 0;
 }
